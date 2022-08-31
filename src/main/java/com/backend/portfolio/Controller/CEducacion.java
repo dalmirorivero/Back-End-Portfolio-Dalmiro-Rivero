93c9584cec2a9a3cc6677df68176ba73699c8b1b
Dalmiro Rivero <dalmiro.rivero1@gmail.com>
2022-08-31 19:13:42 -0300

    EDITABLE EDUCACION

---
+++ src/main/java/com/backend/portfolio/Controller/CEducacion.java
@@ -0,0 +1,98 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.backend.portfolio.Controller;
+
+import com.backend.portfolio.Dto.dtoEducacion;
+import com.backend.portfolio.Entity.Educacion;
+import com.backend.portfolio.Security.Controller.Mensaje;
+import com.backend.portfolio.Service.SEducacion;
+import java.util.List;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Dalmi
+ */
+@RestController
+@RequestMapping("/educacion")
+@CrossOrigin(origins = "http://localhost:4200")
+public class CEducacion {
+    @Autowired
+    SEducacion sEducacion;
+    
+    @GetMapping("/lista")
+    public ResponseEntity<List<Educacion>> list(){
+        List<Educacion> list = sEducacion.list();
+        return new ResponseEntity(list, HttpStatus.OK);
+    }
+    
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> delete (@PathVariable("id") int id){
+        if(!sEducacion.existsById(id)){
+            return new ResponseEntity(new Mensaje("ID inexistente"), HttpStatus.NOT_FOUND);
+        }
+        sEducacion.delete(id);
+        return new ResponseEntity(new Mensaje("Campo eliminado!"), HttpStatus.OK);
+    }
+    
+    @PostMapping("/create")
+    public ResponseEntity<?> create(@RequestBody dtoEducacion dtoeducacion){
+        if(StringUtils.isBlank(dtoeducacion.getNombreE())){
+            return new ResponseEntity(new Mensaje("Nombre obligatorio"), HttpStatus.BAD_REQUEST);
+        }
+        if(sEducacion.existsByNombreE(dtoeducacion.getNombreE())){
+            return new ResponseEntity(new Mensaje("Nombre existente"), HttpStatus.BAD_REQUEST);
+        }
+        
+        Educacion educacion = new Educacion (
+        dtoeducacion.getNombreE(), dtoeducacion.getDescripcionE());
+        sEducacion.save(educacion);
+        return new ResponseEntity(new Mensaje("Campo creado con exito!"), HttpStatus.OK);
+    }
+    
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> update (@PathVariable("id") int id, @RequestBody dtoEducacion dtoeducacion){
+      if(!sEducacion.existsById(id)){
+          return new ResponseEntity(new Mensaje("ID inexistente"), HttpStatus.NOT_FOUND);
+      } 
+      if(sEducacion.existsByNombreE(dtoeducacion.getNombreE()) && sEducacion.getByNombreE(dtoeducacion.getNombreE()).get().getId() != id){
+          return new ResponseEntity(new Mensaje("Nombre ya utilizado"), HttpStatus.BAD_REQUEST);
+      }
+      if(StringUtils.isBlank(dtoeducacion.getNombreE())){
+          return new ResponseEntity(new Mensaje("Campo obligatorio"), HttpStatus.BAD_REQUEST);
+      }
+      
+      Educacion educacion = sEducacion.getOne(id).get();
+      
+      educacion.setNombreE(dtoeducacion.getNombreE());
+      educacion.setDescripcionE(dtoeducacion.getDescripcionE());
+      
+      sEducacion.save(educacion);
+      
+      return new ResponseEntity(new Mensaje("Campo actualizado con exito!"), HttpStatus.OK);
+    }
+    
+    @GetMapping("/detail/{id}")
+    public ResponseEntity<Educacion> getById(@PathVariable("id")int id){
+        if(!sEducacion.existsById(id)){
+            return new ResponseEntity(new Mensaje("ID Inexistente"), HttpStatus.BAD_REQUEST);
+        }
+        
+        Educacion educacion = sEducacion.getOne(id).get();
+        return new ResponseEntity(educacion, HttpStatus.OK);
+    }
+}
