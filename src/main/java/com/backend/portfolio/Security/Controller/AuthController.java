ded4db1e5917f2bca0a170784455c7d2b7d2f794
Dalmiro Rivero <dalmiro.rivero1@gmail.com>
2022-10-15 01:56:04 -0300

    login

--- src/main/java/com/backend/portfolio/Security/Controller/AuthController.java
+++ src/main/java/com/backend/portfolio/Security/Controller/AuthController.java
@@ -41,7 +41,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin
+@CrossOrigin(origins = "https://frontendportfolio-e6cc4.web.app")
 public class AuthController {
  @Autowired
     PasswordEncoder passwordEncoder;
